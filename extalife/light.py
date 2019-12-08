ee13c858e0d27b4abba5a3099ee9acba40fbdcb2
dgtal1 <27864579+dgtal1@users.noreply.github.com>
2019-12-08 12:54:03 +0100

    Fixes for SLR-22
    Fixes for SLR-22

--- extalife/light.py
+++ extalife/light.py
@@ -60,6 +60,7 @@ class ExtaLifeLight(ExtaLifeChannel, Light):
         """Turn on the switch."""
         data = self.channel_data
         params = dict()
+        rgb = w = None
         if self._supported_flags & SUPPORT_BRIGHTNESS:
             target_brightness = kwargs.get(ATTR_BRIGHTNESS)
 
@@ -72,26 +73,26 @@ class ExtaLifeLight(ExtaLifeChannel, Light):
 
         _LOGGER.debug("white value: %s", kwargs)
 
-        if self._supported_flags & SUPPORT_COLOR:
-            target_color = kwargs.get(ATTR_HS_COLOR)
-
-            # Exta Life colors in SLR22 are 4 bytes: RGBW
-            # HA passes either color or white value information
-            if target_color is not None:
-                # We set it to the target brightness and turn it on
-                w = int(data.get("mode_val")) & 255
-                hs = kwargs.get(ATTR_HS_COLOR)          # should return a tuple (h, s)
-                rgb = color_util.color_hs_to_RGB(*hs)   # returns a tuple (R, G, B)
-                rgbw = (int(rgb[0]) << 24) | (int(rgb[1]) << 16) | (int(rgb[2]) << 8) | int(w)
-                params.update({"mode_val": rgbw})
-                params.update({"mode": 0})      # white component only = false
+        if self._supported_flags & SUPPORT_WHITE_VALUE:
+            if not kwargs.get(ATTR_WHITE_VALUE):
+                w = int(self.channel_data.get("mode_val")) & 255    # default
             else:
                 w = int(kwargs.get(ATTR_WHITE_VALUE)) & 255
-                rgbw = int(data.get("mode_val")) | w            # get RGB from channel data and new value from HA
-                params.update({"mode_val": rgbw})
-                params.update({"mode": 1})  # white component only = true
 
-        #self.action(ExtaLifeAPI.ACTN_TURN_ON)
+        if self._supported_flags & SUPPORT_COLOR:
+            if not kwargs.get(ATTR_HS_COLOR):
+                rgb = int(self.channel_data.get("mode_val"))    # default
+            else:
+                hs = kwargs.get(ATTR_HS_COLOR)  # should return a tuple (h, s)
+                rgb = color_util.color_hs_to_RGB(*hs)  # returns a tuple (R, G, B)
+                rgb = (int(rgb[0]) << 24) | (int(rgb[1]) << 16) | (int(rgb[2]) << 8)
+
+        if self._supported_flags & SUPPORT_WHITE_VALUE and self._supported_flags & SUPPORT_COLOR:
+            # Exta Life colors in SLR22 are 4 bytes: RGBW
+            rgbw = rgb & w
+            params.update({"mode_val": rgbw})
+            params.update({"mode": 0})  # white component only = false
+
         if self.action(ExtaLifeAPI.ACTN_TURN_ON, **params):
             data["power"] = 1
             data.update(params)
@@ -100,9 +101,18 @@ class ExtaLifeLight(ExtaLifeChannel, Light):
     def turn_off(self, **kwargs):
         """Turn off the switch."""
         data = self.channel_data
+        params = dict()
+        mode = data.get("mode")
+        if mode:
+            params.update({"mode": mode})
+        mode_val = data.get("mode_val")
+        if mode_val:
+            params.update({"mode_val": mode_val})
+        value = data.get("value")
+        if value:
+            params.update({"value": value})
 
-        if self.action(ExtaLifeAPI.ACTN_TURN_OFF, value=data.get("value")):
-
+        if self.action(ExtaLifeAPI.ACTN_TURN_OFF, **params):
             data["power"] = 0
             self.schedule_update_ha_state()
 
@@ -123,7 +133,7 @@ class ExtaLifeLight(ExtaLifeChannel, Light):
     def hs_color(self):
         """ Device colour setting """
         # TODO: need some research to implement this for SLR22
-        rgbw = self.channel_data.get("mode_val")
+        rgbw = int(self.channel_data.get("mode_val"))
         rgb = rgbw >> 8
         r = rgb >> 16
         g = (rgb >> 8) & 255
