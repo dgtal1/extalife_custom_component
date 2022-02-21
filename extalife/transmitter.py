67702b67084aff7556a3733abc5d8a0e8662e790
dgtal1 <27864579+dgtal1@users.noreply.github.com>
2022-02-21 11:56:43 +0100

    Update transmitter.py
    * formatter
    * minor fix double callback call in _sync_state_notif_update_callback

--- extalife/transmitter.py
+++ extalife/transmitter.py
@@ -10,7 +10,7 @@ from homeassistant.helpers.dispatcher import (
 from homeassistant.helpers.typing import HomeAssistantType
 
 from .helpers.common import PseudoPlatform
-from .helpers.const import (DOMAIN,  DOMAIN_TRANSMITTER)
+from .helpers.const import DOMAIN, DOMAIN_TRANSMITTER
 
 from .helpers.core import Core
 from .helpers.typing import DeviceManagerType, ExtaLifeTransmitterEventProcessorType
@@ -21,9 +21,11 @@ from .pyextalife import (
     PRODUCT_MANUFACTURER,
     PRODUCT_SERIES,
 )
+
 _LOGGER = logging.getLogger(__name__)
 
-CORE_STORAGE_ID = 'transmitter_mgr'
+CORE_STORAGE_ID = "transmitter_mgr"
+
 
 async def async_setup_entry(hass: HomeAssistantType, config_entry: ConfigEntry):
     """Set up Exta Life transmitters based on existing config."""
@@ -44,6 +46,7 @@ async def async_setup_entry(hass: HomeAssistantType, config_entry: ConfigEntry):
 
     core.pop_channels(DOMAIN_TRANSMITTER)
 
+
 async def async_unload_entry(hass: HomeAssistantType, config_entry: ConfigEntry):
     """Unload Exta Life transmitters based on existing config."""
 
@@ -54,6 +57,7 @@ async def async_unload_entry(hass: HomeAssistantType, config_entry: ConfigEntry)
 
     await manager.unload_transmitters()
     core.storage_remove(CORE_STORAGE_ID)
+
 
 class ExtaLifeTransmitter(PseudoPlatform):
     def __init__(self, config_entry: ConfigEntry, channel_data: dict):
@@ -71,22 +75,18 @@ class ExtaLifeTransmitter(PseudoPlatform):
             self._sync_state_notif_update_callback,
         )
 
-
     async def async_will_remove_from_hass(self) -> None:
         await super().async_will_remove_from_hass()
         self._signal_data_notif_remove_callback()
 
-
     def _sync_state_notif_update_callback(self, data):
+        # pass notification to device for processing
         if self.device:
             _LOGGER.debug("_sync_state_notif_update_callback: %s", data)
             self._device.controller_event(data)
 
-        # pass notification to device for processing
-        if self.device:
-            self._device.controller_event(data)
 
-class TransmitterManager():
+class TransmitterManager:
     def __init__(self, config_entry: ConfigEntry):
         self._core = Core.get(config_entry.entry_id)
         self._hass = self._core.hass
@@ -95,14 +95,13 @@ class TransmitterManager():
 
         self._transmitters = dict()
 
-
     @property
-    def dev_manager(self) -> 'DeviceManager':
+    def dev_manager(self) -> "DeviceManager":
         # return self._hass.data[DOMAIN][DATA_DEV_MANAGER]
         return self._core.dev_manager
 
     async def add(self, channel_data: dict):
-        """ Add transmitter instance to buffer """
+        """Add transmitter instance to buffer"""
         transmitter = ExtaLifeTransmitter(self._config_entry, channel_data)
         data = {id: transmitter.id}
         self._transmitters.update(data)
@@ -110,17 +109,15 @@ class TransmitterManager():
         await self.register_device(transmitter)
         await transmitter.async_added_to_hass()
 
-
     async def register_device(self, transmitter: ExtaLifeTransmitter):
-        """ Register transmitter in Device Registry """
+        """Register transmitter in Device Registry"""
 
-        device = await self.dev_manager.async_add(transmitter.device_type, transmitter.device_info)
+        device = await self.dev_manager.async_add(
+            transmitter.device_type, transmitter.device_info
+        )
         transmitter.assign_device(device)
 
-
     async def unload_transmitters(self):
-        """ Unload transmitters: cleanup, unregister signals etc """
+        """Unload transmitters: cleanup, unregister signals etc"""
         for id, transmitter in self._transmitters:
             await transmitter.async_will_remove_from_hass()
-
-
